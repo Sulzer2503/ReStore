675a311a6e9cb84793e1a9508904ea722c02a7fd
Romans Maksjutovs <romans.maksjutovs@damen.com>
2023-09-24 17:24:20 +0200

    End of section 3

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,36 @@
+import Catalog from '../../features/catalog/Catalog';
+import Header from './Header';
+import { Container, CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import { useState } from 'react';
+
+function App() {
+
+  const [darkMode, setDarkMode] = useState(false)
+
+  const palleteType = darkMode ? 'dark' : 'light'
+
+  const theme = createTheme({
+    palette: {
+      mode: palleteType,
+      background: {
+        default : palleteType === 'light' ? '#eaeaea' : '#121212'
+      }
+    }
+  })
+
+  function HandleThemeChange() {
+    setDarkMode(!darkMode)
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <Header darkMode={darkMode} handleThemeChange={HandleThemeChange}/>
+      <Container>
+        <Catalog />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
